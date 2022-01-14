b12939fd50d929c2860a74d0813d728905916eb8
Your Name <sergio.jn024@gmail.com>
2022-01-13 23:33:33 -0500

    Config

--- Models/TipoCuenta.cs
+++ Models/TipoCuenta.cs
@@ -7,5 +7,10 @@ namespace PersonalFi.Models
 {
     public class TipoCuenta
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+
     }
 }
