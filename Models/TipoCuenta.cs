753d6b099c40e77e93baf82dbc49e6296d456abe
Your Name <sergio.jn024@gmail.com>
2022-01-13 23:56:46 -0500

    Cf

--- Models/TipoCuenta.cs
+++ Models/TipoCuenta.cs
@@ -9,7 +9,8 @@ namespace PersonalFi.Models
     public class TipoCuenta
     {
         public int Id { get; set; }
-        [Required(ErrorMessage ="El Campo Nombre es requerido")]
+        [Required(ErrorMessage ="El Campo {0} es requerido")]
+        [StringLength(maximumLength:50,MinimumLength =3,ErrorMessage ='La longitud del campo{0} debe {2} y {1} caracteres')]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
