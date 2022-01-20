cfb62ac69d2ef4da10143e4005b02d8f93232e77
Your Name <sergio.jn024@gmail.com>
2022-01-20 16:51:25 -0500

    Cf

---
+++ 2/SFP/SFP/Controllers/TipoCuentaController.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using PersonalFi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalFi.Controllers
+{
+    public class TipoCuentaController : Controller
+    {
+       
+        public TipoCuentaController()
+        {
+           
+        }
+        public IActionResult Crear()
+        { 
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+            return View();
+        }
+    }
+}
