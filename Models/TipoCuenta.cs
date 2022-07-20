345f055663ddd63d561cf3a5e0d3574db0e753fa
Sergio Narvasta <sergio.jn024@gmail.com>
2022-07-19 23:30:57 -0500

    Valid

--- Models/TipoCuenta.cs
+++ Models/TipoCuenta.cs
@@ -10,12 +10,16 @@ namespace PersonalFi.Models
     public class TipoCuenta /* : IValidatableObject*/
     {
         public int Id { get; set; }
+
         [Required(ErrorMessage ="El Campo {0} es requerido")]
         [StringLength(maximumLength:50,MinimumLength =3,ErrorMessage ="La longitud del campo{0} debe {2} y {1} caracteres")]
         [Display(Name ="Nombre del tipo cuenta")]
         [PrimeraLetraMayus]
+
         public string Nombre { get; set; }
+
         public int UsuarioId { get; set; }
+
         public int Orden { get; set; }
 
         //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
